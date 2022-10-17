a7c51e0c639a529364d230e2a36e61f67e760c18
sugurukabashima <icloud_i4s@me.com>
2022-10-17 22:48:00 +0900

    commit

--- atc085/b_kagamimochi.cpp
+++ atc085/b_kagamimochi.cpp
@@ -3,3 +3,21 @@
 using namespace std;
 
 constexpr int M = 101;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> exist(M, 0);
+
+    for (int i = 0; i < N; i++) {
+        int d;
+        cin >> d;
+        exist[d] = 1;
+    }
+    int result = 0;
+    for (auto val: exist) {
+        result += val;
+    }
+
+    cout << result << endl;
+}
